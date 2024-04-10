833f3e653194e7debfbe2b19d2fb1135a4b5b2b1
MiguelMolina01 <miguelangelcruz5033@gmail.com>
2024-04-09 21:32:23 -0600

    Guia 4
    Guia 4 terminada

---
+++ Guia 4/guia 4.sql
@@ -0,0 +1,477 @@
+insert into departamentos values
+-- idDepartamento, departamento, pais 
+	('AH', 'Ahuachapán', 'El Salvador'),
+	('CA', 'Cabañas', 'El Salvador'),
+	('CH', 'Chalatenango', 'El Salvador'),
+	('CU', 'Cuscatlán', 'El Salvador'),
+	('LL', 'La Libertad', 'El Salvador'),
+	('LP', 'La Paz', 'El Salvador'),
+	('LU', 'La Unión', 'El Salvador'),
+	('MO', 'Morazán', 'El Salvador'),
+	('SA', 'Santa Ana', 'El Salvador'),
+	('SM', 'San Miguel', 'El Salvador'),
+	('SS', 'San Salvador', 'El Salvador'),
+	('SV', 'San Vicente', 'El Salvador'),
+	('SO', 'Sonsonate', 'El Salvador'),
+	('US', 'Usulután', 'El Salvador');
+
+insert into municipios values
+-- idMunicipio, municipio, idDepartamento
+	('AHN', 'Ahuachapán Norte', 'AH'),
+	('AHC', 'Ahuachapán Centro', 'AH'),
+	('AHS', 'Ahuachapán Sur', 'AH'),
+	('CAE', 'Cabañas Este', 'CA'),
+	('CAO', 'Cabañas Oeste', 'CA'),
+	('CHN', 'Chalatenango Norte', 'CH'),
+	('CHC', 'Chalatenango Centro', 'CH'),
+	('CHS', 'Chalatenango Sur', 'CH'),
+	('CUN', 'Cuscatlán Norte', 'CU'),
+	('CUS', 'Cuscatlán Sur', 'CU'),
+	('LLN', 'La Libertad Norte', 'LL'),
+	('LLC', 'La Libertad Centro', 'LL'),
+	('LLO', 'La Libertad Oeste', 'LL'),
+	('LLE', 'La Libertad Este', 'LL'),
+	('LLS', 'La Libertad Sur', 'LL'),
+	('LLT', 'La Libertad Costa', 'LL'),
+	('LPO', 'La Paz Oeste', 'LP'),
+	('LPC', 'La Paz Centro', 'LP'),
+	('LPE', 'La Paz Este', 'LP'),
+	('LUN', 'La Unión Norte', 'LU'),
+	('LUS', 'La Unión Sur', 'LU'),
+	('MON', 'Morazán Norte', 'MO'),
+	('MOS', 'Morazán Sur', 'MO'),
+	('SAN', 'Santa Ana Norte', 'SA'),
+	('SAC', 'Santa Ana Centro', 'SA'),
+	('SAE', 'Santa Ana Este', 'SA'),
+	('SAO', 'Santa Ana Oeste', 'SA'),
+	('SMN', 'San Miguel Norte', 'SM'),
+	('SMC', 'San Miguel Centro', 'SM'),
+	('SMO', 'San Miguel Oeste', 'SM'),
+	('SSN', 'San Salvador Norte', 'SS'),
+	('SSO', 'San Salvador Oeste', 'SS'),
+	('SSE', 'San Salvador Este', 'SS'),
+	('SSC', 'San Salvador Centro', 'SS'),
+	('SSS', 'San Salvador Sur', 'SS'),
+	('SVN', 'San Vicente Norte', 'SV'),
+	('SVS', 'San Vicente Sur', 'SV'),
+	('SON', 'Sonsonate Norte', 'SO'),
+	('SOC', 'Sonsonate Centro', 'SO'),
+	('SOE', 'Sonsonate Este', 'SO'),
+	('SOO', 'Sonsonate Oeste', 'SO'),
+	('USN', 'Usulután Norte', 'US'),
+	('USE', 'Usulután Este', 'US'),
+	('USO', 'Usulután Oeste', 'US');
+
+insert into distritos values
+-- idDistrito, distrito, idMunicipio
+-- Ahuachapan
+	('AHN01', 'Atiquizaya', 'AHN'),
+	('AHN02', 'El Refugio', 'AHN'),
+	('AHN03', 'San Lorenzo', 'AHN'),
+	('AHN04', 'Turín', 'AHN'),
+	('AHC01', 'Ahuachapán', 'AHC'),
+	('AHC02', 'Apaneca', 'AHC'),
+	('AHC03', 'Concepción de Ataco', 'AHC'),
+	('AHC04', 'Tacuba', 'AHC'),
+	('AHS01', 'Guaymango', 'AHS'),
+	('AHS02', 'Jujutla', 'AHS'),
+	('AHS03', 'San Francisco Menéndez', 'AHS'),
+	('AHS04', 'San Pedro Puxtla', 'AHS'),
+-- Cabañas
+	('CAE01', 'Sensuntepeque', 'CAE'),
+	('CAE02', 'Victoria', 'CAE'),
+	('CAE03', 'Dolores', 'CAE'),
+	('CAE04', 'Guacotecti', 'CAE'),
+	('CAE05', 'San Isidro', 'CAE'),
+	('CAO01', 'Ilobasco', 'CAO'),
+	('CAO02', 'Tejutepeque', 'CAO'),
+	('CAO03', 'Jutiapa', 'CAO'),
+	('CAO04', 'Cinquera', 'CAO'),
+-- Chalatenango
+	('CHN01', 'La Palma', 'CHN'),
+	('CHN02', 'Citalá', 'CHN'),
+	('CHN03', 'San Ignacio', 'CHN'),
+	('CHC01', 'Nueva Concepción', 'CHC'),
+	('CHC02', 'Tejutla', 'CHC'),
+	('CHC03', 'La Reina', 'CHC'),
+	('CHC04', 'Agua Caliente', 'CHC'),
+	('CHC05', 'Dulce Nombre de María', 'CHC'),
+	('CHC06', 'El Paraíso', 'CHC'),
+	('CHC07', 'San Fernando', 'CHC'),
+	('CHC08', 'San Francisco Morazán', 'CHC'),
+	('CHC09', 'San Rafael', 'CHC'),
+	('CHC10', 'Santa Rita', 'CHC'),
+	('CHS01', 'Chalatenango', 'CHS'),
+	('CHS02', 'Arcatao', 'CHS'),
+	('CHS03', 'Azacualpa', 'CHS'),
+	('CHS04', 'Comalapa', 'CHS'),
+	('CHS05', 'Concepción Quezaltepeque', 'CHS'),
+	('CHS06', 'El Carrizal', 'CHS'),
+	('CHS07', 'La Laguna', 'CHS'),
+	('CHS08', 'Las Vueltas', 'CHS'),
+	('CHS09', 'Nombre de Jesús', 'CHS'),
+	('CHS10', 'Nueva Trinidad', 'CHS'),
+	('CHS11', 'Ojos de Agua', 'CHS'),
+	('CHS12', 'Potonico', 'CHS'),
+	('CHS13', 'San Antonio de La Cruz', 'CHS'),
+	('CHS14', 'San Antonio Los Ranchos', 'CHS'),
+	('CHS15', 'San Francisco Lempa', 'CHS'),
+	('CHS16', 'San Isidro Labrador', 'CHS'),
+	('CHS17', 'San José Cancasque', 'CHS'),
+	('CHS18', 'San Miguel de Mercedes', 'CHS'),
+	('CHS19', 'San José Las Flores', 'CHS'),
+	('CHS20', 'San Luis del Carmen', 'CHS'),
+-- Cuscatlán
+	('CUN01', 'Suchitoto', 'CUN'),
+	('CUN02', 'San José Guayabal', 'CUN'),
+	('CUN03', 'Oratorio de Concepción', 'CUN'),
+	('CUN04', 'San Bartolomé Perulapía', 'CUN'),
+	('CUN05', 'San Pedro Perulapán', 'CUN'),
+	('CUS01', 'Cojutepeque', 'CUS'),
+	('CUS02', 'San Rafael Cedros', 'CUS'),
+	('CUS03', 'Candelaria', 'CUS'),
+	('CUS04', 'Monte San Juan', 'CUS'),
+	('CUS05', 'El Carmen', 'CUS'),
+	('CUS06', 'San Cristobal', 'CUS'),
+	('CUS07', 'Santa Cruz Michapa', 'CUS'),
+	('CUS08', 'San Ramón', 'CUS'),
+	('CUS09', 'El Rosario', 'CUS'),
+	('CUS10', 'Santa Cruz Analquito', 'CUS'),
+	('CUS11', 'Tenancingo', 'CUS'),
+-- La Libertad
+	('LLN01', 'Quezaltepeque', 'LLN'),
+	('LLN02', 'San Matías', 'LLN'),
+	('LLN03', 'San Pablo Tacachico', 'LLN'),
+	('LLC01', 'San Juan Opico', 'LLC'),
+	('LLC02', 'Ciudad Arce', 'LLC'),
+	('LLO01', 'Colón', 'LLO'),
+	('LLO02', 'Jayaque', 'LLO'),
+	('LLO03', 'Sacacoyo', 'LLO'),
+	('LLO04', 'Tepecoyo', 'LLO'),
+	('LLO05', 'Talnique', 'LLO'),
+	('LLE01', 'Antiguo Cuscatlán', 'LLE'),
+	('LLE02', 'Huizúcar', 'LLE'),
+	('LLE03', 'Nuevo Cuscatlán', 'LLE'),
+	('LLE04', 'San José Villanueva', 'LLE'),
+	('LLE05', 'Zaragoza', 'LLE'),
+	('LLS01', 'Comasagua', 'LLS'),
+	('LLS02', 'Santa Tecla', 'LLS'),
+	('LLT01', 'Chiltiupán', 'LLT'),
+	('LLT02', 'Jicalapa', 'LLT'),
+	('LLT03', 'La Libertad', 'LLT'),
+	('LLT04', 'Tamanique', 'LLT'),
+	('LLT05', 'Teotepeque', 'LLT'),
+-- La Paz
+	('LPO01', 'Cuyultitan', 'LPO'),
+	('LPO02', 'Olocuilta', 'LPO'),
+	('LPO03', 'San Juan Talpa', 'LPO'),
+	('LPO04', 'San Luis Talpa', 'LPO'),
+	('LPO05', 'San Pedro Masahuat', 'LPO'),
+	('LPO06', 'Tapalhuaca', 'LPO'),
+	('LPO07', 'San Francisco Chinameca', 'LPO'),
+	('LPC01', 'El Rosario', 'LPC'),
+	('LPC02', 'Jerusalén', 'LPC'),
+	('LPC03', 'Mercedes La Ceiba', 'LPC'),
+	('LPC04', 'Paraíso de Osorio', 'LPC'),
+	('LPC05', 'San Antonio Masahuat', 'LPC'),
+	('LPC06', 'San Emigdio', 'LPC'),
+	('LPC07', 'San Juan Tepezontes', 'LPC'),
+	('LPC08', 'San Luís La Herradura', 'LPC'),
+	('LPC09', 'San Miguel Tepezontes', 'LPC'),
+	('LPC10', 'San Pedro Nonualco', 'LPC'),
+	('LPC11', 'Santa María Ostuma', 'LPC'),
+	('LPC12', 'Santiago Nonualco', 'LPC'),
+	('LPE01', 'San Juan Nonualco', 'LPE'),
+	('LPE02', 'San Rafael Obrajuelo', 'LPE'),
+	('LPE03', 'Zacatecoluca', 'LPE'),
+-- La Unión
+	('LUN01', 'Anamorós', 'LUN'),
+	('LUN02', 'Bolivar', 'LUN'),
+	('LUN03', 'Concepción de Oriente', 'LUN'),
+	('LUN04', 'El Sauce', 'LUN'),
+	('LUN05', 'Lislique', 'LUN'),
+	('LUN06', 'Nueva Esparta', 'LUN'),
+	('LUN07', 'Pasaquina', 'LUN'),
+	('LUN08', 'Polorós', 'LUN'),
+	('LUN09', 'San José La Fuente', 'LUN'),
+	('LUN10', 'Santa Rosa de Lima', 'LUN'),
+	('LUS01', 'Conchagua', 'LUS'),
+	('LUS02', 'El Carmen', 'LUS'),
+	('LUS03', 'Intipucá', 'LUS'),
+	('LUS04', 'La Unión', 'LUS'),
+	('LUS05', 'Meanguera del Golfo', 'LUS'),
+	('LUS06', 'San Alejo', 'LUS'),
+	('LUS07', 'Yayantique', 'LUS'),
+	('LUS08', 'Yucuaiquín', 'LUS'),
+-- Morazán
+	('MON01', 'Arambala', 'MON'),
+	('MON02', 'Cacaopera', 'MON'),
+	('MON03', 'Corinto', 'MON'),
+	('MON04', 'El Rosario', 'MON'),
+	('MON05', 'Joateca', 'MON'),
+	('MON06', 'Jocoaitique', 'MON'),
+	('MON07', 'Meanguera', 'MON'),
+	('MON08', 'Perquín', 'MON'),
+	('MON09', 'San Fernando', 'MON'),
+	('MON10', 'San Isidro', 'MON'),
+	('MON11', 'Torola', 'MON'),
+	('MOS01', 'Chilanga', 'MOS'),
+	('MOS02', 'Delicias de Concepción', 'MOS'),
+	('MOS03', 'El Divisadero', 'MOS'),
+	('MOS04', 'Gualococti', 'MOS'),
+	('MOS05', 'Guatajiagua', 'MOS'),
+	('MOS06', 'Jocoro', 'MOS'),
+	('MOS07', 'Lolotiquillo', 'MOS'),
+	('MOS08', 'Osicala', 'MOS'),
+	('MOS09', 'San Carlos', 'MOS'),
+	('MOS10', 'San Francisco Gotera', 'MOS'),
+	('MOS11', 'San Simón', 'MOS'),
+	('MOS12', 'Sensembra', 'MOS'),
+	('MOS13', 'Sociedad', 'MOS'),
+	('MOS14', 'Yamabal', 'MOS'),
+	('MOS15', 'Yoloaiquín', 'MOS'),
+-- Santa Ana
+	('SAN01', 'Masahuat', 'SAN'),
+	('SAN02', 'Metapán', 'SAN'),
+	('SAN03', 'Santa Rosa Guachipilín', 'SAN'),
+	('SAN04', 'Texistepeque', 'SAN'),
+	('SAC01', 'Santa Ana', 'SAC'),
+	('SAE01', 'Coatepeque', 'SAE'),
+	('SAE02', 'El Congo', 'SAE'),
+	('SAO01', 'Candelaria de la Frontera', 'SAO'),
+	('SAO02', 'Chalchuapa', 'SAO'),
+	('SAO03', 'El Porvenir', 'SAO'),
+	('SAO04', 'San Antonio Pajonal', 'SAO'),
+	('SAO05', 'San Sebastián Salitrillo', 'SAO'),
+	('SAO06', 'Santiago de La Frontera', 'SAO'),
+-- San Miguel
+	('SMN01', 'Ciudad Barrios', 'SMN'),
+	('SMN02', 'Sesori', 'SMN'),
+	('SMN03', 'Nuevo Edén de San Juan', 'SMN'),
+	('SMN04', 'San Gerardo', 'SMN'),
+	('SMN05', 'San Luis de La Reina', 'SMN'),
+	('SMN06', 'Carolina', 'SMN'),
+	('SMN07', 'San Antonio del Mosco', 'SMN'),
+	('SMN08', 'Chapeltique', 'SMN'),
+	('SMC01', 'San Miguel', 'SMC'),
+	('SMC02', 'Comacarán', 'SMC'),
+	('SMC03', 'Uluazapa', 'SMC'),
+	('SMC04', 'Moncagua', 'SMC'),
+	('SMC05', 'Quelepa', 'SMC'),
+	('SMC06', 'Chirilagua', 'SMC'),
+	('SMO01', 'Chinameca', 'SMO'),
+	('SMO02', 'Nueva Guadalupe', 'SMO'),
+	('SMO03', 'Lolotique', 'SMO'),
+	('SMO04', 'San Jorge', 'SMO'),
+	('SMO05', 'San Rafael Oriente', 'SMO'),
+	('SMO06', 'El Tránsito', 'SMO'),
+-- San Salvador
+	('SSN01', 'Aguilares', 'SSN'),
+	('SSN02', 'El Paisnal', 'SSN'),
+	('SSN03', 'Guazapa', 'SSN'),
+	('SSO01', 'Apopa', 'SSO'),
+	('SSO02', 'Nejapa', 'SSO'),
+	('SSE01', 'Ilopango', 'SSE'),
+	('SSE02', 'San Martín', 'SSE'),
+	('SSE03', 'Soyapango', 'SSE'),
+	('SSE04', 'Tonacatepeque', 'SSE'),
+	('SSC01', 'Ayutuxtepeque', 'SSC'),
+	('SSC02', 'Mejicanos', 'SSC'),
+	('SSC03', 'San Salvador', 'SSC'),
+	('SSC04', 'Cuscatancingo', 'SSC'),
+	('SSC05', 'Ciudad Delgado', 'SSC'),
+	('SSS01', 'Panchimalco', 'SSS'),
+	('SSS02', 'Rosario de Mora', 'SSS'),
+	('SSS03', 'San Marcos', 'SSS'),
+	('SSS04', 'Santo Tomás', 'SSS'),
+	('SSS05', 'Santiago Texacuangos', 'SSS'),
+-- San Vicente
+	('SVN01', 'Apastepeque', 'SVN'),
+	('SVN02', 'Santa Clara', 'SVN'),
+	('SVN03', 'San Ildefonso', 'SVN'),
+	('SVN04', 'San Esteban Catarina', 'SVN'),
+	('SVN05', 'San Sebastián', 'SVN'),
+	('SVN06', 'San Lorenzo', 'SVN'),
+	('SVN07', 'Santo Domingo', 'SVN'),
+	('SVS01', 'San Vicente', 'SVS'),
+	('SVS02', 'Guadalupe', 'SVS'),
+	('SVS03', 'Verapaz', 'SVS'),
+	('SVS04', 'Tepetitán', 'SVS'),
+	('SVS05', 'Tecoluca', 'SVS'),
+	('SVS06', 'San Cayetano Istepeque', 'SVS'),
+-- Sonsonate
+	('SON01', 'Juayúa', 'SON'),
+	('SON02', 'Nahuizalco', 'SON'),
+	('SON03', 'Salcoatitán', 'SON'),
+	('SON04', 'Santa Catarina Masahuat', 'SON'),
+	('SOC01', 'Sonsonate', 'SOC'),
+	('SOC02', 'Sonzacate', 'SOC'),
+	('SOC03', 'Nahulingo', 'SOC'),
+	('SOC04', 'San Antonio del Monte', 'SOC'),
+	('SOC05', 'Santo Domingo de Guzmán', 'SOC'),
+	('SOE01', 'Izalco', 'SOE'),
+	('SOE02', 'Armenia', 'SOE'),
+	('SOE03', 'Caluco', 'SOE'),
+	('SOE04', 'San Julián', 'SOE'),
+	('SOE05', 'Cuisnahuat', 'SOE'),
+	('SOE06', 'Santa Isabel Ishuatán', 'SOE'),
+	('SOO01', 'Acajutla', 'SOO'),
+-- Usulután
+	('USN01', 'Santiago de María', 'USN'),
+	('USN02', 'Alegría', 'USN'),
+	('USN03', 'Berlín', 'USN'),
+	('USN04', 'Mercedes Umaña', 'USN'),
+	('USN05', 'Jucuapa', 'USN'),
+	('USN06', 'El triunfo', 'USN'),
+	('USN07', 'Estanzuelas', 'USN'),
+	('USN08', 'San Buenaventura', 'USN'),
+	('USN09', 'Nueva Granada', 'USN'),
+	('USE01', 'Usulután', 'USE'),
+	('USE02', 'Jucuarán', 'USE'),
+	('USE03', 'San Dionisio', 'USE'),
+	('USE04', 'Concepción Batres', 'USE'),
+	('USE05', 'Santa María', 'USE'),
+	('USE06', 'Ozatlán', 'USE'),
+	('USE07', 'Tecapán', 'USE'),
+	('USE08', 'Santa Elena', 'USE'),
+	('USE09', 'California', 'USE'),
+	('USE10', 'Ereguayquín', 'USE'),
+	('USO01', 'Jiquilisco', 'USO'),
+	('USO02', 'Puerto El Triunfo', 'USO'),
+	('USO03', 'San Agustín', 'USO'),
+	('USO04', 'San Francisco Javier', 'USO');
+    
+    insert into direcciones (linea1, linea2, idDistrito, codigoPostal) values
+	('Col Madera, Calle 1, #1N', 'Frente al parque', 'SON02', '02311'),  -- 1					
+	('Barrio El Caldero, Av 2, #2I', 'Frente al amate', 'SOE01', '02306'), -- 2
+	('Res El Cangrejo, Av 3, #3A', 'Frente a la playa', 'SOO01', '02302'), -- 3
+	('Barrio El Centro, Av 4, #4S', 'Frente a catedral', 'SOC01', '02301'), -- 4
+	('Col La Frontera, Calle 5, #5G', 'Km 10', 'AHS03', '02113'), -- 5
+	('Res Buenavista, Calle 6, #6A', 'Contiguo a Alcaldia', 'SAC01', '02201'), -- 6
+	('Res Altavista, Av 7, #7S', 'Contiguo al ISSS', 'SSC03', '01101'), -- 7
+	('Col Las Margaritas, Pje 20, #2-4', 'Junto a ANDA', 'AHS01', '02114'),-- 8
+	('Urb Las Magnolias, Pol 21, #2-6', 'Casa de esquina', 'LLO01', '01115'),-- 9
+	('Caserio Florencia, 3era Calle, #5', 'Casa rosada', 'SON01', '02305');-- 10
+    
+insert into cargos values
+	('1','SysAdmin'),
+	('2','Bibliotecario'),
+	('3','Ordenanza');
+    
+    insert into empleados values
+-- idEmpleado, nombresEmpleado, apellidosEmpleado, duiEmpleado, isssEmpleado,  fechaNacEmpleado, telefonoEmpleado, correoEmpleado, idCargo, idDireccion
+	('1', 'Juan Carlos', 'Rodas Gonzalez', '04523695-5', '906325698', '1995-01-01', '6532-4526', 'juan@hotmail.com', '1', '1'),
+	('2', 'Diego Franciso', 'Sanchez Castro', '04321098-7', '963852741', '1990-02-02', '7895-5698', 'diego@gmail.com', '2', '2'),
+	('3', 'Raul Edgardo', 'Del Valle Garcia', '03210987-4', '321654987', '1980-03-03', '6598-2548', 'raul@outlook.com', '3', '3'),
+	('4', 'Mary Carmen', 'Perez de Hernandez', '06789012-1', '951753258', '1985-04-04', '7965-2526', 'may@yahoo.com', '2', '4'),
+	('5', 'Kevin Angel', 'Luke de Quevedo', '06776012-5', '871563254', '1986-03-07', '7645-2526', 'kaq@yahoo.com', '3', '5');
+    
+    insert into lectores values
+    -- idLector, nombreLector, ApellidoLector
+    ('1', 'Diego Alejandro', 'Herrera Moreno'),
+    ('2', 'Laura Camila', ' Castro Escobar'),
+    ('3', 'Jose Maria', 'Ramírez Tovar'),
+    ('4', 'Gabriel Antonio', 'García Beltrán'),
+    ('5', 'García Beltrán', 'Romero Jiménez');
+    
+    insert into historialPrestamos values
+    -- idHistorial, fechaEntregado, fechaRecibido
+     ('1', '2024-01-12', '2024-02-12'),
+     ('2', '2024-01-23', '2024-02-10'),
+     ('3', '2024-02-04', '2024-02-20'),
+     ('4', '2024-03-10', '2024-03-21'),
+     ('5', '2024-03-15', '2024-04-01');
+     
+     insert into prestamos values
+     -- idPrestamo,fechaPrestamo, fechaDevolucion, idHistorial, idEmpleado, idLector 
+     ('1', '2024-01-12', '2024-02-14', '1', '2', '1'),
+     ('2', '2024-01-23', '2024-02-10','2', '4', '2'),
+     ('3', '2024-02-04', '2024-02-25','3', '2', '3'),
+     ('4', '2024-03-10', '2024-03-25','4', '2', '4'),
+     ('5', '2024-03-15', '2024-04-01','5', '4', '5');
+     
+     insert into moras values
+     -- idMora, cantidadDias, moraDia, cantidadmora, idPrestamo
+     ('1', '2', '1.00', '2.00','1'),
+     ('2', '0', '1.00','0.00','2'),
+     ('3', '5', '1.00', '5.00','3'),
+     ('4', '3', '1.00', '3.00','4'),
+     ('5', '0', '1.00', '0.00','5');
+     
+      insert into autores values
+      -- idAutor, nombreAutor, apellidoAutor
+        (1, 'Gabriel', 'García Márquez'),
+        (2, 'Antoine', 'de Saint-Exupéry'),
+        (3, 'Yuval Noah', 'Harari'),
+        (4, 'Isabel', 'Allende'), 
+        (5, 'Mario', 'Vargas Llosa');
+      
+      insert into categoriaLibros values
+      -- idCategoria, categoria1, categoria2 
+      ('1', 'Literatura', 'Novela'),
+      ('2', 'infantil', 'Clásicos'),
+	  ('3', 'Realismo', 'Mágico'),
+      ('4', 'Historia', 'Sociedad'),
+      ('5', 'Ciencia','Ficción');
+      
+      insert into editoriales values
+      -- idEditorial, editorial, fechaPublicacion
+      ('1', 'Alfaguara'),
+	  ('2', 'Salamandra'),
+      ('3', 'Penguin Random House'),
+      ('4', 'Debate'),
+      ('5', 'Debolsillo');
+      
+      insert into condicionLibros values
+      -- idCondicionLibro, condicion
+      ('1', 'Nuevo'),
+      ('2', 'Excelente'),
+      ('3', 'Bueno'),
+      ('4', 'Aceptable'),
+      ('5', 'Deteriorado');
+      
+     insert into Libros values 
+     -- ISBN, libro, estado,fechaPublicacion, idEditorial, idCategoria,idCondicionLibro
+	  ('9788439732471', 'Cien años de soledad', 'Disponible','2017-04-06', '3', '3', '1'),
+      ('9780156013925', 'El Principito', 'Disponible','2001-09-04', '3', '2', '2'),
+      ('9788499926223', 'Sapiens: A Brief History of Humankind', 'Disponible','2016-09-27', '4', '4', '2'),
+      ('9788499926711', 'Homo Deus: A Brief History of Tomorrow', 'Disponible','2017-01-31','4','4', '3'),
+	  ('9781644732717', '21 lecciones para el siglo XXI', 'Disponible','2020-07-21', '5', '4', '4');
+      
+insert into prestamoLibro values
+-- idPrestamoLibro ,ISBN ,idPrestamo
+	  ('1', '9788439732471', '1'),
+	  ('2', '9780156013925', '2'),
+	  ('3', '9788499926223', '3'),
+	  ('4', '9788499926711', '4'),
+	  ('5', '9781644732717', '5');
+
+insert into autoreslibros values
+  -- idAutoresLibros, idAutor, ISBN
+	  ('1', '1', '9788439732471'),
+	  ('2', '2', '9780156013925'),
+	  ('3', '3', '9788499926223'),
+	  ('4', '3', '9788499926711'),
+	  ('5', '3', '9781644732717');
+      
+    -- Consultas 
+    SELECT * FROM biblioteca.municipios;
+    SELECT * FROM biblioteca.departamentos;
+    SELECT * FROM biblioteca.distritos;
+    SELECT * FROM biblioteca.direcciones;
+    SELECT * FROM biblioteca.cargos;
+    SELECT * FROM biblioteca.empleados;
+    SELECT * FROM biblioteca.lectores;
+    SELECT * FROM biblioteca.historialprestamos;
+    SELECT * FROM biblioteca.prestamos;
+    SELECT * FROM biblioteca.moras;
+    SELECT * FROM biblioteca.autores;
+    SELECT * FROM biblioteca.categorialibros;
+    SELECT * FROM biblioteca.editoriales;
+    SELECT * FROM biblioteca.condicionlibros;
+    SELECT * FROM biblioteca.libros;
+    SELECT * FROM biblioteca.prestamolibro;
+    SELECT * FROM biblioteca.autoreslibros;
+    
+    
+    
